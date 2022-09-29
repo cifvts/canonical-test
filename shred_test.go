e58252d7d4dec931eb349f4acfd63e97a491417a
Andrea Agosti <andrea.agosti@accolade.com>
2022-09-29 16:32:26 +0200

    Add more test cases

--- shred_test.go
+++ shred_test.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"os"
 	"testing"
 )
 
 func TestShred(t *testing.T) {
+	bak := out
+	out = new(bytes.Buffer)
+	defer func() { out = bak }()
+
 	var testPath = "/tmp/testme"
 	var file, err = os.OpenFile(testPath, os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
@@ -20,7 +25,7 @@ func TestShred(t *testing.T) {
 	var ret = shred(testPath)
 
 	if ret != 0 {
-		t.Log("Shred return 1")
+		t.Log("Shred return 1, should be 0")
 		t.Fail()
 	}
 
@@ -30,6 +35,21 @@ func TestShred(t *testing.T) {
 	}
 
 	if err.Error() != "open /tmp/testme: no such file or directory" {
+		t.Fail()
+	}
+}
+
+func TestShredNoFile(t *testing.T) {
+	bak := out
+	out = new(bytes.Buffer)
+	defer func() { out = bak }()
+
+	var testPath = "/tmp/testme"
+
+	var ret = shred(testPath)
+
+	if ret == 0 {
+		t.Log("Shred return 0, should be 1")
 		t.Fail()
 	}
 }
